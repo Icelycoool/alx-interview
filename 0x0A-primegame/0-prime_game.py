9d0abf3646dd92ffbe200c4d2a4da4daeb98602a
Icelycoool <mohammedcali@proton.me>
2024-12-13 02:31:28 +0300

    Completed primegame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Prime game"""
+def isWinner(x, nums):
+    """
+    Determine the winner of the prime game.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): Array of n values for each round.
+
+    Returns:
+        str: The name of the player that won the most rounds, or None if tied.
+    """
+    if not nums or x < 1:
+        return None
+
+    # Find the maximum number in nums
+    max_n = max(nums)
+
+    # Sieve of Eratosthenes to find all primes up to max_n
+    primes = [True] * (max_n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_n + 1, i):
+                primes[j] = False
+
+    # Precompute the number of primes less than or equal to each number
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    # Determine the winner for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        # If the number of primes up to n is odd, Maria wins; otherwise, Ben wins
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
